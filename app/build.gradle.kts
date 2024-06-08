345ecc9f89406d6deca11e96eae829368b7c3e96
Alexey Glukharev <lwengine@gmail.com>
2024-06-07 19:15:03 -0700

    bump lib versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
@@ -64,7 +64,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose-android:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose-android:2.8.1")
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
